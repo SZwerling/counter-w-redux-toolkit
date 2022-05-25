d19fdce322b7ee3fe60cb8928ace64aba3473d45
unknown <sazwerling@gmail.com>
2022-05-24 19:01:29 -0500

    update state w redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { produceWithPatches } from 'immer';
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'; //to call from reducer
-import { increment, decrement, incrementByAmount } from './counter'; //import the actions from the reducer
+import { selectSong } from './counter'; //import the actions from the reducer
 
 
 const App = () => {
-    const { count } = useSelector(state => state.counter) //destructure the count var from the counter reducer
-    const dispatch = useDispatch() //use to call any action from any reducer
+    const { count, mySongs, selectedSong } = useSelector(state => state.counter) //destructure the count var from the counter reducer
+    const dispatch = useDispatch()
+    
+    console.log(selectedSong)
+    
+    const listOfSongs = mySongs.map((song) => {
+            return(
+                <li onClick={() => dispatch(selectSong(song))} key={song.title}>{song.title}</li>
+            )
+    })
+
+   
+   
     return(
+       
         <div className="app">
             <h1>The count is: {count}</h1>
-            <button onClick={() => dispatch(increment())}>increment</button>
-            <button onClick={() => dispatch(decrement())}>decrement</button>
-            <button onClick={() => dispatch(incrementByAmount(22))}>add 22</button>
+            <ul>{listOfSongs}</ul>
+            <div>{selectedSong.payload.title}</div>
+            
         </div>
     )
 }
--- src/counter.js
+++ src/counter.js
@@ -2,6 +2,24 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   count: 0,
+  selectedSong: {
+    title: "song one",
+    duration: "4:32"
+  },
+  mySongs: [
+    {
+      title: "song one",
+      duration: "4:32"
+    },
+    {
+      title: "Number Two of the Songs",
+      duration: "3:21"
+    },
+    {
+      title: "three",
+      duration: "3:33"
+    }
+  ]
 }
 
 export const counterSlice = createSlice({
@@ -13,18 +31,23 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.count += 1
+      state.count += 1;
     },
-    decrement: (state) => {  //action inside reducer
-      state.count -= 1
+    decrement: (state) => {
+      state.count -=1;
     },
-    incrementByAmount: (state, action) => {  //action inside reducer
-      state.count += action.payload
-    },
+    selectSong: (state, action) => {
+      return {...state, selectedSong: action}
+    }
   },
 })
 
+// return {
+//   ...state,
+//   value: 123,
+// }
+
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions  //export actions to call in components
+export const { increment, decremen, selectSong } = counterSlice.actions  //export actions to call in components
 
 export default counterSlice.reducer  //export reducer to use in redux store
